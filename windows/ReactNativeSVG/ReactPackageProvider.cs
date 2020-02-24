1d7ee0a4c203c890e3aa0aea736042dbbf3bc7bb
Jérôme Diaz <j.diaz@eurelis.com>
2020-02-24 14:58:51 +0100

    test autolink

---
+++ windows/ReactNativeSVG/ReactPackageProvider.cs
@@ -0,0 +1,14 @@
+using Microsoft.ReactNative;
+using Microsoft.ReactNative.Managed;
+
+namespace ReactNativeSVG
+{
+    public sealed class ReactPackageProvider : IReactPackageProvider
+    {
+        public void CreatePackage(IReactPackageBuilder packageBuilder)
+        {
+            packageBuilder.AddAttributedModules();
+            packageBuilder.AddViewManagers();
+        }
+    }
+}
